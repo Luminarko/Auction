791727bf875e98e3f199ead7abf5b7837dc4e0ab
Imrich Zeleňák <92807683+Luminarko@users.noreply.github.com>
2022-03-23 19:35:36 +0100

    Add files via upload

---
+++ auction/js/data.js
@@ -0,0 +1,64 @@
+const data = {
+    art1: 
+        {
+            name: "Mona Lisa",
+            description: "Mona Lisa, též označovaná La Gioconda, je patrně nejslavnější portrét všech dob, který na počátku 16. století namaloval italský geniální umělec a stavitel Leonardo da Vinci. Vytvořil jej během svého pobytu ve Florencii a odvezl jej s sebou při svém útĕku z Říma do Francie v roce 1516.",
+            author: "Leonardo da Vinci",
+            date: "1503",
+            images: ["paintings/mona_lisa.jpg","paintings/mona_lisa.jpg"],
+            price: "5000Kč"
+        },
+
+    art2: 
+        {
+            name: "Hvězdná noc",
+            description: "Hvězdná noc je obraz od nizozemského postimpresionisty Vincenta van Gogha, na němž převládají barvy noci. Olejomalbu namaloval v červnu 1889. Kompozice zachycuje pohled z pokojového okna sanatoria v jihofrancouzském městě Saint-Rémy-de-Provence.",
+            author: "Vincent Van Gogh",
+            date: "1889",
+            images: ["paintings/Starry_night.jpg","paintings/Starry_night.jpg"],
+            price: "5000Kč"
+        },
+
+    art3: 
+        {
+            name: "Výkřik",
+            description: "Výkřik je expresionistický obraz, nejvýznamnější dílo norského malíře Edvarda Muncha. Zachycuje výkřikem znetvořenou postavu s lysou lebkou na pozadí stylizované krajiny a krvavého nebe.",
+            author: "Edvard Munch",
+            date: "1893",
+            images: ["paintings/The_Scream.jpg","paintings/The_Scream.jpg"],
+            price: "5000Kč"
+        },
+    
+
+    art4: 
+        {
+            name: "Americká gotika",
+            description: "Výkřik je expresionistický obraz, nejvýznamnější dílo norského malíře Edvarda Muncha. Zachycuje výkřikem znetvořenou postavu s lysou lebkou na pozadí stylizované krajiny a krvavého nebe.",
+            author: "Grant DeVolson Wood",
+            date: "1930",
+            images: ["paintings/American_Gothic.jpg","paintings/American_Gothic.jpg"],
+            price: "5000Kč"
+        },
+    
+
+    art5:
+        {
+            name: "A Friend in Need",
+            description: "Výkřik je expresionistický obraz, nejvýznamnější dílo norského malíře Edvarda Muncha. Zachycuje výkřikem znetvořenou postavu s lysou lebkou na pozadí stylizované krajiny a krvavého nebe.",
+            author: "Cassius Marcellus Coolidge",
+            date: "1903",
+            images: ["paintings/Dogs_Playing_Poker.jpg","paintings/The_Scream.jpg"],
+            price: "5000Kč"
+        },
+    
+    
+    art6: 
+        {
+            name: "The Son of Man",
+            description: "Výkřik je expresionistický obraz, nejvýznamnější dílo norského malíře Edvarda Muncha. Zachycuje výkřikem znetvořenou postavu s lysou lebkou na pozadí stylizované krajiny a krvavého nebe.",
+            author: "René Magritte",
+            date: "1964",
+            images: ["paintings/The_Son_of_Man.jpg","paintings/The_Son_of_Man.jpg"],
+            price: "5000Kč"
+        },
+}
---
+++ auction/js/script.js
@@ -0,0 +1,18 @@
+const gallery = document.getElementById("gallery");
+const details = document.getElementById("details");
+
+for(let index in data){
+    console.log(index);
+    let item = data[index];
+    let itemElement = document.createElement("div");
+    itemElement.innerHTML = `<img src="${item.images[0]}"><h2 id="name">${item.name}</h2><p> ${item.description}</p><p> ${item.author}</p><p class="price">${item.price}</p>`;
+    gallery.appendChild(itemElement);
+    
+    itemElement.onclick = () => {
+        details.innerHTML = `<div class="background"><div class="deta"><img src="${item.images[0]}"><img src="${item.images[1]}"></div></div>`;
+        details.style.display = "flex";
+    };
+    details.onclick = () => {
+        details.style.display = "none";
+    };
+}
